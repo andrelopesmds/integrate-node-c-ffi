174db9d1e0134151baaa04f312b8143cfe648ca4
andrelopesmds <andrelopesmds@gmail.com>
2020-08-06 23:57:57 +0300

    Move performance test to folder.

--- runK6.sh
+++ runK6.sh
@@ -3,7 +3,7 @@
 route=$1
 
 if [ $route = "c" ] || [ $route = "js" ]; then
-    docker run --env route=$route --net=host -i loadimpact/k6 run - <k6-script.js
+    docker run --env route=$route --net=host -i loadimpact/k6 run - <performance-test/k6-script.js
 else
     echo "Path parameter is required! It must be either 'c' or 'js'"
 fi
